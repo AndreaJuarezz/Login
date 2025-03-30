9f2e33e3e052f6eefd975c5f3a882a3282d72e82
AndreaJuarezz <Andreanava370@gmail.com>
2025-03-30 17:08:50 -0600

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-green-600">Sesión iniciada con éxito</h1>
+      </div>
+    );
+  }
+  
