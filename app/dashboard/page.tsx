3dc2e5260297ab0381e75ca9c55405dd9ea6f551
AndreaJuarezz <Andreanava370@gmail.com>
2025-03-30 19:51:42 -0600

    correccion css

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,32 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Dashboard() {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-3xl font-bold text-green-600">Sesión iniciada con éxito</h1>
-      </div>
-    );
-  }
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-3xl font-bold text-green-600 mb-2">
+        Sesión iniciada con éxito
+      </h1>
+      <p className="text-lg text-gray-700">
+        Bienvenido, administrador
+      </p>
+    </div>
+  );
+}
+
+ 
+
+
+  
+
   
